244724135838e57e18a9285eacacf34cd581f56b
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-08-03 15:11:42 +0500

    Variables Second Example

--- variables.js
+++ variables.js
@@ -1,1 +1,11 @@
-let city = 'Porbandar';
+var city = "Porbandar";
+let x = 'Geeta';
+
+console.log(city);
+console.log(x);
+
+var city = 'Lubumbashi';
+let country = 'Congo';
+
+console.log(city);
+console.log(country); 
