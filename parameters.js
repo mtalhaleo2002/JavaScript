036df2c55a2cdd5290b004eaac7a4fa8f0a35625
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-08-03 15:25:03 +0500

    Parameters

---
+++ parameters.js
@@ -0,0 +1,26 @@
+function give(input) {
+  return input;
+}
+function parameters() {
+  return {
+    give: give,
+    input: 'What is g?'
+  };
+}
+console.log(parameters().give(parameters().input));
+// Output: What is g?
+
+function echo(name){
+  return name;
+}
+console.log(echo('Greta'));
+console.log(echo('CO2'));
+// Output: Greta, CO2
+
+function add(a, b) {
+  return a + b;
+}
+add(5, 10);
+console.log(add(5, 10));
+
+// Parameters in functions
