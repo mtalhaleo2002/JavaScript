53d2d2706926f522c3abbd9c0658e974f52fc6df
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-08-03 17:05:25 +0500

    Number In JavaScript

---
+++ numbers.js
@@ -0,0 +1,14 @@
+let x1 = 6;
+let x2 = 2;
+let x3 = x1 + x2;
+let x4 = x1 - x2;
+let x5 = x1 * x2;
+let x6 = x1 / x2;
+let x7 = x1 % x2;
+console.log(x3); // Output: 8
+console.log(x4); // Output: 4
+console.log(x5); // Output: 12
+console.log(x6); // Output: 3
+console.log(x7); // Output: 0
+
+// Number In JavaScript
