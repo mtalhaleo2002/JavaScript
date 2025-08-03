ca73d3ff293a9f62fcbb21ece96d7942b6a3032e
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-08-03 15:29:31 +0500

    What is x?

--- strings.js
+++ strings.js
@@ -11,3 +11,12 @@ console.log(greet('Ada'));
 console.log(greet('Grace'));
 
 // Strings in JavaScript
+
+function whereIs(name) {
+  return 'Where is ' + name + '?';
+}
+
+let x = whereIs('Jacky');
+console.log(x);
+
+// What is x?
