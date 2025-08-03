b152c0aba7e3fcad15e5b50f40b7721f901cff5a
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-08-03 15:28:43 +0500

    Strings

---
+++ strings.js
@@ -0,0 +1,13 @@
+function praise(name) {
+  return name + ' is great!';
+}
+console.log(praise('JavaScript'));
+
+function greet(name){
+ return 'Hello ' + name + '!';
+}
+
+console.log(greet('Ada'));
+console.log(greet('Grace'));
+
+// Strings in JavaScript
