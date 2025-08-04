bc428d5a2baf1e05671b5ff340622a16efa5770f
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-08-04 12:26:27 +0500

    ParseInt Function and Addition from String

--- numbers.js
+++ numbers.js
@@ -13,20 +13,24 @@ console.log(x7); // Output: 0
 
 // Number In JavaScript
 
-function add(n1,n2){
-  return n1 + n2;
+function add(n1, n2) {
+    return n1 + n2;
 }
-function subtract(n1,n2){
-  return n1 - n2;
+
+function subtract(n1, n2) {
+    return n1 - n2;
 }
-function multiply(n1,n2){
-  return n1 * n2;
+
+function multiply(n1, n2) {
+    return n1 * n2;
 }
-function divide(n1,n2){
-  return n1 / n2;
+
+function divide(n1, n2) {
+    return n1 / n2;
 }
-function modulus(n1,n2){
-  return n1 % n2;
+
+function modulus(n1, n2) {
+    return n1 % n2;
 }
 console.log(add(5, 3)); // Output: 8
 console.log(subtract(5, 3)); // Output: 2
@@ -54,11 +58,12 @@ console.log(s1); // Output: 7
 // Increment and Decrement Operators
 
 
-function toFahrenheit(number){
-return (number * 9/5) + 32
- }
-function toCelsius(number){
-return (number - 32) * 5/9
+function toFahrenheit(number) {
+    return (number * 9 / 5) + 32
+}
+
+function toCelsius(number) {
+    return (number - 32) * 5 / 9
 }
 console.log(toFahrenheit(0)); // Output: 32
 console.log(toCelsius(32)); // Output: 0
@@ -70,8 +75,8 @@ let n2 = 20 % 3;
 console.log(n1); // Output: 1
 console.log(n2); // Output: 2
 
-function onesDigit(number){
-  return number % 10;
+function onesDigit(number) {
+    return number % 10;
 }
 
 console.log(onesDigit(123)); // Output: 3
@@ -84,8 +89,8 @@ let xa2 = (3 + 4) * 2;
 console.log(xa1); // Output: 11
 console.log(xa2); // Output: 
 
-function mean(n1,n2){
- return (n1 + n2)/2;
+function mean(n1, n2) {
+    return (n1 + n2) / 2;
 }
 
 console.log(mean(10, 20)); // Output: 15
@@ -99,12 +104,12 @@ let y2 = Math.pow(10, 3);
 console.log(y1); // Output: 3
 console.log(y2); // Output: 1000
 
-function hypotenuse(n1,n2){
- let s1 = Math.pow(n1,2);
- let s2 = Math.pow(n2,2);
- let result = s1 + s2;
- return Math.sqrt(result);
-  
+function hypotenuse(n1, n2) {
+    let s1 = Math.pow(n1, 2);
+    let s2 = Math.pow(n2, 2);
+    let result = s1 + s2;
+    return Math.sqrt(result);
+
 }
 
 console.log(hypotenuse(3, 4)); // Output: 5
@@ -118,8 +123,8 @@ let max = Math.max(3, 9, 2);
 console.log(min); // Output: 5
 console.log(max); // Output: 9
 
-function midrange(a,b,c){
- return (Math.min(a,b,c) + Math.max(a,b,c)) /2;
+function midrange(a, b, c) {
+    return (Math.min(a, b, c) + Math.max(a, b, c)) / 2;
 }
 console.log(midrange(1, 5, 3)); // Output: 3
 console.log(midrange(10, 20, 30)); // Output: 20
@@ -132,7 +137,7 @@ console.log(PiValue); // Output: 3.141592653589793
 console.log(eValue); // Output: 2.718281828459045
 
 function area(number) {
- return Math.PI * number * number;
+    return Math.PI * number * number;
 }
 console.log(area(5)); // Output: 78.53981633974483
 console.log(area(10)); // Output: 314.1592653589793
@@ -150,7 +155,7 @@ console.log(c); // Output: 5
 console.log(d); // Output: 5
 
 function round100(x) {
-  return Math.round(x / 100) * 100;
+    return Math.round(x / 100) * 100;
 }
 
 console.log(round100(123)); // Output: 100
@@ -159,12 +164,48 @@ console.log(round100(789)); // Output: 800
 
 // Rounding Functions
 
-function dice(){
- let x = Math.random() * 6;
-  return Math.ceil(x);
+function dice() {
+    let x = Math.random() * 6;
+    return Math.ceil(x);
 }
 
 console.log(dice());
 
 // Random Number Generation
 
+let b1 = parseInt('19', 10);
+console.log(b1); // Output: 19
+let b2 = parseInt('+19', 10);
+console.log(b2); // Output: 19
+let n3 = parseInt('-19', 10);
+console.log(n3); // Output: -19
+let n4 = parseInt('19 Grad', 10);
+console.log(n4); // Output: 19
+let n5 = parseInt('19.9', 10);
+console.log(n5); // Output: 19
+let n6 = parseInt('019', 10);
+console.log(n6); // Output: 19
+let n7 = parseInt(' 19', 10);
+console.log(n7); // Output: 19
+let n8 = parseInt('19abc', 10);
+console.log(n8); // Output: 19
+let n9 = parseInt('abc19', 10);
+console.log(n9); // Output: NaN
+
+function add(stringName) {
+    let firstValue = parseInt(stringName, 10);
+    let findIndex = stringName.indexOf('+');
+    let AfterPlus = stringName.substr(findIndex + 1);
+    let secondValue = parseInt(AfterPlus, 10);
+    return firstValue + secondValue;
+}
+// console.log(add("19+1"));
+// console.log(add("19 + 1"));
+// console.log(add("19+ 1"));
+// console.log(add("19 +1"));
+// console.log(add("19+1 Grad"));
+// console.log(add("19 +1 Grad"));
+// console.log(add("19+ 1 Grad"));
+// These are all console works in Browser Console Or Online JavaScript Editors
+
+// ParseInt Function and Addition from String
