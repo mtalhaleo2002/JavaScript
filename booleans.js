07e000b72d82d9219d6584f5ca5d1181fb4876ec
Mohammad Talha <m.talhaleo2002@gmail.com>
2025-08-11 16:20:18 +0500

    Function Check Boolean Values Are Equal Or Unequal

--- booleans.js
+++ booleans.js
@@ -1,5 +1,5 @@
 let x1 = true && false;
-console.log(x1); 
+console.log(x1);
 
 let x2 = !x1;
 console.log(x2);
@@ -15,9 +15,9 @@ console.log(x5);
 
 // Boolean Operators
 
-function nand(a,b){
- let some = a && b;
- return !some;
+function nand(a, b) {
+  let some = a && b;
+  return !some;
 }
 
 let x6 = nand(true, false);
@@ -29,10 +29,9 @@ console.log(nand(false, true));
 
 // This function returns the logical AND of two boolean values.
 
-
-function nor(a,b){
- let some = a || b;
-return !some;
+function nor(a, b) {
+  let some = a || b;
+  return !some;
 }
 
 console.log(nor(true, false));
@@ -42,9 +41,8 @@ console.log(nor(false, true));
 
 // This function returns the logical OR of two boolean values.
 
-
-function xor(a, b){
-  return a && !b || !a && b;
+function xor(a, b) {
+  return (a && !b) || (!a && b);
 }
 
 console.log(xor(true, false));
@@ -52,28 +50,37 @@ console.log(xor(false, true));
 console.log(xor(true, true));
 // This function returns the logical XOR of two boolean values.
 
-function equals(a,b){
+function equals(a, b) {
   return a === b;
 }
 // This function checks if two boolean values are equal.
 
-function equals(a,b,c){
+function equals(a, b, c) {
   return a === b && b === c;
 }
 // This function checks if three boolean values are equal.
 
-function isEven(a){
+function isEven(a) {
   return a % 2 === 0;
 }
 // This function checks if a number is even.
 
-function unequal(a,b,c){
-  return a !== b && b !== c && a!==c;
+function unequal(a, b, c) {
+  return a !== b && b !== c && a !== c;
 }
 // This function checks if three boolean values are not equal.
 
-function isThreeDigit(a){
+function isThreeDigit(a) {
   return a >= 100 && a < 1000;
 }
 // This function checks if  number is greater than or equal to 100 and less than 1000.
 
+function equals(a, b) {
+  if (a === b) {
+    return "EQUAL";
+  } else {
+    return "UNEQUAL";
+  }
+}
+
+// This function checks if two boolean values are equal and returns "EQUAL" or "UNEQUAL".
